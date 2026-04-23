7b804f567ffab615cffb7697d2b6e20c42610bef
unknown <minna.nordqvist@edu.turkuamk.fi>
2026-04-23 12:25:17 +0300

    getting started

---
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -0,0 +1,94 @@
+package org.example.project
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SheetValue
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberStandardBottomSheetState
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.painterResource
+
+import mablibreproject.composeapp.generated.resources.Res
+import mablibreproject.composeapp.generated.resources.compose_multiplatform
+import org.maplibre.compose.map.MaplibreMap
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview
+fun App() {
+    val coroutineScope = rememberCoroutineScope()
+    val sheetState = rememberStandardBottomSheetState(
+        initialValue = SheetValue.PartiallyExpanded
+    )
+
+    val scaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = sheetState
+    )
+    MaterialTheme {
+        BottomSheetScaffold(
+            scaffoldState = scaffoldState,
+            sheetSwipeEnabled = true,
+            sheetPeekHeight = 260.dp,
+            sheetMaxWidth = Dp.Unspecified,
+
+
+            sheetContent = {
+
+
+            },
+            topBar = {
+                TopAppBar(
+                    colors = topAppBarColors(
+
+                        titleContentColor = Color.Black,
+                    ),
+                    title = {
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            textAlign = TextAlign.Center,
+                            text = "Föli Zone"
+                        )
+                    }
+                )
+            },  // Main content
+            content = { paddingValues ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues),
+                    contentAlignment = Alignment.Center
+                ) {
+                    MaplibreMap()
+                }
+
+            },
+
+            modifier = Modifier.fillMaxWidth()
+
+        )
+
+    }
+}
+
