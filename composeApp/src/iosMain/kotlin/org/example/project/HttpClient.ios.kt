dee0dbcb1de2d360ad6b70f6aaa019553c0fa59e
unknown <minna.nordqvist@edu.turkuamk.fi>
2026-05-18 12:39:49 +0300

    HTTP CLIENT IOS

--- composeApp/src/iosMain/kotlin/org/example/project/HttpClient.ios.kt
+++ composeApp/src/iosMain/kotlin/org/example/project/HttpClient.ios.kt
@@ -2,6 +2,56 @@ package org.example.project
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.darwin.Darwin
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.UserAgent
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.header
+import io.ktor.http.URLProtocol
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
-actual val client: HttpClient
-    get() = HttpClient(Darwin)
+actual val client = HttpClient(Darwin) {
+    engine {
+        configureRequest {
+            setAllowsCellularAccess(true)
+        }
+    }
+
+    install(HttpTimeout) {
+        socketTimeoutMillis = 60_000
+        requestTimeoutMillis = 60_000
+    }
+
+    install(Logging) {
+        logger = Logger.DEFAULT
+        level = LogLevel.ALL
+
+    }
+    install(UserAgent) {
+        agent = "MapLibreProject Android"
+    }
+
+    defaultRequest {
+        val rnds = (0..100).random()
+        url {
+            protocol = URLProtocol.HTTPS
+            host = "data.foli.fi"
+            parameters.append("?hash_id=", "$rnds")
+        }
+        header("MapLibreProject-Android-Header", "Hello")
+    }
+
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = false
+            isLenient = true
+            ignoreUnknownKeys = true
+            explicitNulls = false
+        })
+    }
+}
