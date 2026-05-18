ddf535d2d2ea6a05245b600ec6909e7231dac671
unknown <minna.nordqvist@edu.turkuamk.fi>
2026-05-18 16:26:09 +0300

    Autorefresh ominaisuus lisätty. Funktio stopAutoRefresh() ei vielä käytössä

--- composeApp/src/commonMain/kotlin/org/example/project/BusViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/BusViewModel.kt
@@ -3,6 +3,8 @@ package org.example.project
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.ktor.http.HttpStatusCode
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -10,6 +12,7 @@ import org.example.project.data.Response
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import org.example.project.data.SiriApi
+import kotlin.time.Duration.Companion.minutes
 
 data class BusListUIState(
     val busList:  List<Response.Bus> = emptyList(),
@@ -21,7 +24,11 @@ class BusViewModel : ViewModel(), KoinComponent {
     val uiState: StateFlow<BusListUIState> = _uiState
     private val _linesList = MutableStateFlow<List<String>>(emptyList())
     val linesList:  StateFlow<List<String>> = _linesList
+
+    private var refreshJob: Job? = null
+
     fun getBusList(stop_code: String?) {
+        refreshJob?.cancel()
         val lineList = mutableListOf<String>()
         viewModelScope.launch() {
             siriApi.getBusList(stop_code)?.let { busList ->
@@ -30,13 +37,33 @@ class BusViewModel : ViewModel(), KoinComponent {
             }
             _uiState.value.busList.forEach{ bus ->  lineList.add(bus.lineref)}
             _linesList.value = lineList.distinct()
+            startAutoRefreshTimer(stop_code)
 
         }
     }
 
-    suspend fun getResponseStatus(stop_code: String?) : HttpStatusCode{
+    suspend fun getResponseStatus(stop_code: String?) : HttpStatusCode {
         return siriApi.getResponseStatus(stop_code)
 
     }
 
+    private fun startAutoRefreshTimer(stop_code: String?) {
+        // Cancel previous timer just to be safe
+        refreshJob?.cancel()
+
+        refreshJob = viewModelScope.launch {
+            // Wait for exactly 1 minute
+            delay(1.minutes)
+
+            // Loop back to fetch fresh data
+            getBusList(stop_code)
+        }
+    }
+
+
+    fun stopAutoRefresh() {
+        refreshJob?.cancel()
+    }
+
 }
+
